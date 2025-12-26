58bdf6c706c5b6feb48f83966c43cd98c3fa6e16
Fayez <muhammedfayezmh@gmail.com>
2025-12-26 15:56:54 +0530

    Fetch Movie Data from the backend

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useState, useEffect } from "react"; //React Hook for State
+import axios from "axios";
 
 // Material
 import {
@@ -24,40 +25,55 @@ export default function Home() {
   // const [currentTheme, setCurrentTheme] = useState("light");
   const dispatch = useDispatch();
   const currentTheme = useSelector(selectTheme).activeTheme;
+  const [movies, setMovies] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     dispatch(getActiveTheme());
+    fetchData();
   }, []);
 
   const theme = useTheme();
 
-  const movies = [
-    {
-      name: "Avengers",
-      img: "https://m.media-amazon.com/images/I/91hIN+dNYUL.jpg",
-      desc: "Directed By Joss Whedon",
-    },
-    {
-      name: "Terminator",
-      img: "https://townsquare.media/site/295/files/2019/10/Terminator-Orion.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
-      desc: "Directed By James Cameron",
-    },
-    {
-      name: "Inception",
-      img: "https://images5.alphacoders.com/112/1122037.jpg",
-      desc: "Directed By Chris Nolan",
-    },
-    {
-      name: "Jurassic Park",
-      img: "https://i.ytimg.com/vi/Rc_i5TKdmhs/maxresdefault.jpg",
-      desc: "Directed By Steven Spielberg",
-    },
-    {
-      name: "Superman",
-      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxGnS5AO1k3OpZnSm3tRpSE6QMwI6nD1wZAcbPrTsNa9FZQcBttmU_WNWdTX2IpZu2Dmca9g&s=10",
-      desc: "Directed by James Gunn",
-    },
-  ];
+  async function fetchData() {
+    try {
+      const response = await axios.get("/api/v1/get/movies");
+      console.log(response);
+
+      setMovies(response.data.response);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      console.log("finally");
+    }
+  }
+
+  // const movies = [
+  //   {
+  //     name: "Avengers",
+  //     img: "https://m.media-amazon.com/images/I/91hIN+dNYUL.jpg",
+  //     desc: "Directed By Joss Whedon",
+  //   },
+  //   {
+  //     name: "Terminator",
+  //     img: "https://townsquare.media/site/295/files/2019/10/Terminator-Orion.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+  //     desc: "Directed By James Cameron",
+  //   },
+  //   {
+  //     name: "Inception",
+  //     img: "https://images5.alphacoders.com/112/1122037.jpg",
+  //     desc: "Directed By Chris Nolan",
+  //   },
+  // {
+  //   name: "Jurassic Park",
+  //   img: "https://i.ytimg.com/vi/Rc_i5TKdmhs/maxresdefault.jpg",
+  //   desc: "Directed By Steven Spielberg",
+  // },
+  //   {
+  //     name: "Superman",
+  //     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxGnS5AO1k3OpZnSm3tRpSE6QMwI6nD1wZAcbPrTsNa9FZQcBttmU_WNWdTX2IpZu2Dmca9g&s=10",
+  //     desc: "Directed by James Gunn",
+  //   },
+  // ];
 
   return (
     <>
@@ -76,11 +92,11 @@ export default function Home() {
             <Grid container spacing={2} direction="row" justifyContent="center">
               {movies ? (
                 movies.map((movie) => (
-                  <Grid size={4}>
+                  <Grid key={movie._id} size={4}>
                     <CustomCard
                       name={movie.name}
-                      image={movie.img}
-                      description={movie.desc}
+                      image={movie.image}
+                      description={movie.description}
                     />
                   </Grid>
                 ))
